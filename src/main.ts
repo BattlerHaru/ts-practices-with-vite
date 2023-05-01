146f2cdb6d5ec4ddfce719dbd0636878bd824996
BattlerHaru <kronk.sdance@gmail.com>
2023-05-01 15:55:18 -0600

    feat: :sparkles: Inyeccion de dependencias con principio de sustitucion

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,10 @@ import './style.css'
 
 // import { name, age, isValid } from "./bases/01-types"
 // import { studentC, students } from "./bases/02-objects"
-import { onePiece } from './bases/03-classes'
+// import { onePiece } from './bases/03-classes'
+import { squirtle } from './bases/04-injections'
 
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <h1>${onePiece.name} - ${onePiece.episodes}</h1>
+  <h1>${squirtle.name}</h1>
 `
