38dc704da47dff2055601bb717d1a65e8449a298
BattlerHaru <kronk.sdance@gmail.com>
2023-05-01 13:38:33 -0600

    feat: :sparkles: async & await con axios y interface para pokeapi

--- src/bases/03-classes.ts
+++ src/bases/03-classes.ts
@@ -1,3 +1,6 @@
+import axios from "axios";
+import { PokeapiResponse } from "../interfaces/pokeapi-response.interface";
+
 // export class Anime {
 //     public episodes: number;
 //     constructor(episodes: number) {
@@ -28,9 +31,19 @@ export class Anime {
     private cover() {
         return `${this.imageUrl}`;
     }
+
+    async getPokemon(pokeId: number): Promise<string> {
+        const { data } = await axios.get<PokeapiResponse>(`https://pokeapi.co/api/v2/pokemon/${pokeId}`);
+
+        console.log(data.name)
+
+        return data.name;
+    }
 }
 
 export const onePiece = new Anime(1, "One Piece", 1057)
 
-onePiece.episodes += 1;
-onePiece.allData();
+// onePiece.episodes += 1;
+// onePiece.allData();
+
+onePiece.getPokemon(7);
